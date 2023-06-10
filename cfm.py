b25c2fcbee60e55defa2ca37741497ecc488b672
Muhammad Farooq <88403419+farooq9092@users.noreply.github.com>
2023-06-10 20:28:08 +0500

    Update cfm.py

--- cfm.py
+++ cfm.py
@@ -1,22 +1,9 @@
 import streamlit as st
 import pandas as pd
-import base64
-import requests
-import io
 
-# Function to download the CSV file from GitHub
-def download_csv(csv_file_path):
-    response = requests.get(csv_file_path)
-    content = response.content.decode("utf-8")
-    df = pd.read_csv(io.StringIO(content))
-    return df
-
-# Function to save the updated data to the CSV file on GitHub
-def save_data(df, csv_file_path):
-    csv_data = df.to_csv(index=False)
-    b64 = base64.b64encode(csv_data.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{https://github.com/farooq9092/cmf/blob/main/charity_fund_data.csv}">Download CSV File</a>'
-    st.markdown(href, unsafe_allow_html=True)
+def create_csv(data):
+    df = pd.DataFrame(data)
+    df.to_csv('charity_fund_data.csv', index=False)
 
 def main():
     st.title("Monthly Charity Fund for Poor People")
@@ -30,16 +17,8 @@ def main():
         if not distributor_name or not medicine_name or not price:
             st.error("Please fill in all fields.")
         else:
-            # Download the current CSV file from GitHub
-            csv_file_path = "https://github.com/farooq9092/cmf/blob/main/charity_fund_data.csv"
-            df = download_csv(csv_file_path)
-            
-            # Add the new data to the DataFrame
-            new_data = {"Distributor Name": distributor_name, "Medicine Name": medicine_name, "Price": price}
-            df = df.append(new_data, ignore_index=True)
-            
-            # Save the updated DataFrame back to the CSV file on GitHub
-            save_data(df, csv_file_path)
+            new_data = {"Distributor Name": [distributor_name], "Medicine Name": [medicine_name], "Price": [price]}
+            create_csv(new_data)
             
             st.success("Data saved successfully!")
 
