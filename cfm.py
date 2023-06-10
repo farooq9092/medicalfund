a38f0a31f262362673796610f5d823a8709a8dcb
Muhammad Farooq <88403419+farooq9092@users.noreply.github.com>
2023-06-10 18:36:28 +0500

    Update cfm.py

--- cfm.py
+++ cfm.py
@@ -1,35 +1,25 @@
-import csv
 import streamlit as st
+import csv
 
-# Function to save the data of people receiving medicine
-def save_data(name, medicine_name, price):
-    with open("charity_fund_data.csv", "a", newline="") as file:
+def save_data(distributor_name, medicine_name, price):
+    with open("charity_data.csv", "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([name, medicine_name, price])
+        writer.writerow([distributor_name, medicine_name, price])
 
-# Function to display the Streamlit app
 def main():
     st.title("Monthly Charity Fund for Poor People")
-    st.markdown("Enter the details of the medicine distribution:")
-
-    # Input fields for the distributor
-    name = st.text_input("Distributor Name")
+    st.markdown("Please enter the following details:")
+    
+    distributor_name = st.text_input("Distributor Name")
     medicine_name = st.text_input("Medicine Name")
     price = st.number_input("Price")
+    
+    if st.button("Submit"):
+        if not distributor_name or not medicine_name or not price:
+            st.error("Please fill in all fields.")
+        else:
+            save_data(distributor_name, medicine_name, price)
+            st.success("Data saved successfully!")
 
-    # Save data when the "Add" button is clicked
-    if st.button("Add"):
-        save_data(name, medicine_name, price)
-        st.success("Data saved successfully!")
-
-    # Display the data table for verification and checking records
-    st.markdown("## Records")
-    with open("charity_fund_data.csv", "r") as file:
-        reader = csv.reader(file)
-        data = list(reader)
-        st.table(data)
-
-# Run the Streamlit app
 if __name__ == "__main__":
     main()
-
