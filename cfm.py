83a71d5a68c94e33751b85a631ddffb5ae011c37
Muhammad Farooq <88403419+farooq9092@users.noreply.github.com>
2023-06-10 21:01:39 +0500

    Update cfm.py

--- cfm.py
+++ cfm.py
@@ -1,11 +1,20 @@
 import csv
 import streamlit as st
+import base64
 
 # Function to save the data of people receiving medicine
 def save_data(name, medicine_name, price):
     with open("charity_fund_data.csv", "a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([name, medicine_name, price])
+
+# Function to download the CSV file
+def download_csv():
+    with open("charity_fund_data.csv", "r") as file:
+        csv_data = file.read()
+    b64 = base64.b64encode(csv_data.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="charity_fund_data.csv">Download CSV File</a>'
+    return href
 
 # Function to display the Streamlit app
 def main():
@@ -28,6 +37,11 @@ def main():
         reader = csv.reader(file)
         data = list(reader)
         st.table(data)
+
+    # Download the CSV file when the "Download" button is clicked
+    if st.button("Download CSV"):
+        href = download_csv()
+        st.markdown(href, unsafe_allow_html=True)
 
 # Run the Streamlit app
 if __name__ == "__main__":
