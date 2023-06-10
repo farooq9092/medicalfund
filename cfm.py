d6b92f5c163bb2b8d09efbfb715b6e9053864d33
Muhammad Farooq <88403419+farooq9092@users.noreply.github.com>
2023-06-10 20:55:37 +0500

    Update cfm.py

--- cfm.py
+++ cfm.py
@@ -1,46 +1,34 @@
+import csv
 import streamlit as st
-import pandas as pd
-import requests
 
-GITHUB_API = 'https://api.github.com'
-OWNER = 'farooq9092'
-REPO = 'cmf'
-BRANCH = 'main'
-FILE_PATH = 'charity_fund_data.csv'
+# Function to save the data of people receiving medicine
+def save_data(name, medicine_name, price):
+    with open("charity_fund_data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name, medicine_name, price])
 
-def create_csv(data):
-    df = pd.DataFrame(data)
-    csv_data = df.to_csv(index=False)
-    headers = {
-        'Authorization': 'Bearer YOUR_GITHUB_ACCESS_TOKEN',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    url = f'{GITHUB_API}/repos/{OWNER}/{REPO}/contents/{FILE_PATH}'
-    payload = {
-        'message': 'Create CSV file',
-        'content': csv_data,
-        'branch': BRANCH
-    }
-    response = requests.put(url, headers=headers, json=payload)
-    if response.status_code == 201:
-        st.success("CSV file created successfully!")
-    else:
-        st.error("Failed to create CSV file.")
-
+# Function to display the Streamlit app
 def main():
     st.title("Monthly Charity Fund for Poor People")
-    st.markdown("Please enter the following details:")
-    
-    distributor_name = st.text_input("Name")
+    st.markdown("Enter the details of the medicine distribution:")
+
+    # Input fields for the distributor
+    name = st.text_input("Distributor Name")
     medicine_name = st.text_input("Medicine Name")
     price = st.number_input("Price")
-    
-    if st.button("Submit"):
-        if not distributor_name or not medicine_name or not price:
-            st.error("Please fill in all fields.")
-        else:
-            new_data = {"Distributor Name": [distributor_name], "Medicine Name": [medicine_name], "Price": [price]}
-            create_csv(new_data)
 
+    # Save data when the "Add" button is clicked
+    if st.button("Add"):
+        save_data(name, medicine_name, price)
+        st.success("Data saved successfully!")
+
+    # Display the data table for verification and checking records
+    st.markdown("## Records")
+    with open("charity_fund_data.csv", "r") as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        st.table(data)
+
+# Run the Streamlit app
 if __name__ == "__main__":
     main()
